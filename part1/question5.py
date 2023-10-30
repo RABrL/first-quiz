156094ae69b0e05f100f487d8a56c960c7a741b1
RABrL <robin_brito@hotmail.com>
2023-10-30 12:03:34 -0500

    part1/question5 finished

--- part1/question5.py
+++ part1/question5.py
@@ -19,9 +19,11 @@
 # vegetarian integer
 
 sql_create_favorite_foods = """
-
-Your SQL here.
-
+CREATE TABLE favorite_foods (
+    food_id INTEGER PRIMARY KEY,
+    name TEXT,
+    vegetarian INTEGER
+);
 """
 
 # Part 5.B:
@@ -29,9 +31,8 @@ Your SQL here.
 # The test suite will verify the new changes by inserting some new rows. 
 
 sql_alter_tables_with_favorite_food = """
-
-Your SQL here.
-
+ALTER TABLE people ADD COLUMN favorite_food_id INTEGER;
+ALTER TABLE animals ADD COLUMN favorite_food_id INTEGER;
 """
 
 # Part 5.C:
@@ -39,7 +40,7 @@ Your SQL here.
 # THe output should be a list of tuples in the format: (<pet name>, <food name>)
 
 sql_select_all_vegetarian_pets = """
-
-Your SQL here.
-
+SELECT a.name, f.name FROM animals as a
+JOIN favorite_foods as f ON a.favorite_food_id = f.food_id
+WHERE f.name IN ('spinach', 'turnips', 'cough drops');
 """
